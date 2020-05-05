cae632717cb090bb7c32a300f2439f28d3bb31a7
Nadir Hamid <matrix.nad@gmail.com>
2020-05-05 17:24:50 -0600

    update resource page

--- app/resources/views/resources/item.blade.php
+++ app/resources/views/resources/item.blade.php
@@ -9,21 +9,34 @@
             </div>
         </div>
         <div class="row"> 
-            <div class="col s9">
+            <div class="col s8">
                 <div class="markdown">
                     {!! $html !!}
                 </div>
             </div>
-            <div class="col s3 related-bar">
+            <div class="col s4 related-bar">
                 <div class="inner">
                     <h3>Related Items</h3>
                     <hr/>
-                    <ul>
+                    <ul class="related-items">
                     @foreach ($related as $item)
                         <li>
                                 <a href="/resources/{{$item['section']['link']}}/{{$item['item']['link']}}">{{$item['item']['name']}}</a>
                         </li>
                     @endforeach
+                <div class="card horizontal">
+                    <div class="card-stacked">
+                        <div class="card-content">
+                            <h5>
+                                Still need support ?
+                            </h5>
+                            <br/>
+                            <p>We can help you with any questions you may have regarding this post.</p>
+                            <br/>
+                            <a class="btn-custom service-btn" href="/contact">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
                     </div>
             </div>
 
