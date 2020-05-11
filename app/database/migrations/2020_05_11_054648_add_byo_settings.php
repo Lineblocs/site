b311b0350622d0f87da81c8b7da65579d1af07d3
Nadir Hamid <matrix.nad@gmail.com>
2020-05-10 23:47:20 -0600

    add BYO settings

---
+++ app/database/migrations/2020_05_11_054648_add_byo_settings.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddByoSettings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('workspaces', function (Blueprint $table) {
+            //
+            $table->boolean('byo_enabled')->default(FALSE);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('workspaces', function (Blueprint $table) {
+            //
+        });
+    }
+}
