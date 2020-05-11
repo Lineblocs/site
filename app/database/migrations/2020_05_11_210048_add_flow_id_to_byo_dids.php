daee70f143b0f17cab6884af36a26bd32902d3ea
Nadir Hamid <matrix.nad@gmail.com>
2020-05-11 15:01:29 -0600

    add flow to BYO DIDs

---
+++ app/database/migrations/2020_05_11_210048_add_flow_id_to_byo_dids.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFlowIdToByoDids extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('byo_did_numbers', function (Blueprint $table) {
+            //
+            $table->integer('flow_id')->unsigned();
+            $table->foreign('flow_id')->references('id')->on('flows')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('byo_did_numbers', function (Blueprint $table) {
+            //
+        });
+    }
+}
