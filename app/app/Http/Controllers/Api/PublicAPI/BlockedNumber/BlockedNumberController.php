acdec7df1a80252f928a98f1062f618e2194f82b
Nadir Hamid <matrix.nad@gmail.com>
2020-05-17 17:41:02 -0600

    add public API routes

---
+++ app/app/Http/Controllers/Api/PublicAPI/BlockedNumber/BlockedNumberController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api\PublicAPI\BlockedNumber;
+use \App\Http\Controllers\Api\ApiPublicController;
+use \JWTAuth;
+use \Dingo\Api\Routing\Helpers;
+use \Illuminate\Http\Request;
+use \App\User;
+use \App\DIDNumber;
+use \App\DIDNumberTag;
+use \App\UserDebit;
+use \App\Flow;
+use \App\Transformers\DIDNumberTransformer;
+use \App\ThirdParty\NumberService;
+use \App\Helpers\MainHelper;
+use \DB;
+use Mail;
+use Config;
+
+
+
+class BlockedNumberController extends ApiPublicController {
+    public function post(Request $request)
+    {
+    }
+    public function put(Request $request, $extensionId)
+    {
+    }
+
+    public function get(Request $request, $extensionId)
+    {
+    }
+    public function list(Request $request)
+    {
+    }
+    public function delete(Request $request, $extensionId)
+    {
+    }
+
+
+}
+
