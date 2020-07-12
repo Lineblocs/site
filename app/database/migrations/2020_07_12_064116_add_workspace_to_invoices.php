2b976e77176a0b6995bc01fb9f370d44b9c54c5b
Nadir Hamid <matrix.nad@gmail.com>
2020-07-12 00:41:54 -0600

    add workspace columns to billing tables

---
+++ app/database/migrations/2020_07_12_064116_add_workspace_to_invoices.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWorkspaceToInvoices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users_invoices', function (Blueprint $table) {
+            //
+            $table->integer('workspace_id')->unsigned();
+            $table->foreign('workspace_id')->references('id')->on('workspaces');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users_invoices', function (Blueprint $table) {
+            //
+        });
+    }
+}
