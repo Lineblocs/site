4ab5d05c7b62a9ec323bae7ebffc2042e5c10e64
dariialeliukh <dariialelyukh@gmail.com>
2021-01-26 16:31:52 -0600

    Vendor-loaded dir is back, there should be changes to contact us page (new layout design)

---
+++ app/vendor-loaded/messagebird/php-rest-api/src/MessageBird/Resources/PartnerAccount/Accounts.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace MessageBird\Resources\PartnerAccount;
+
+use MessageBird\Common\HttpClient;
+use MessageBird\Objects\PartnerAccount\Account;
+use MessageBird\Resources\Base;
+
+class Accounts extends Base
+{
+    const RESOURCE_NAME = 'child-accounts';
+
+    public function __construct(HttpClient $httpClient)
+    {
+        parent::__construct($httpClient);
+
+        $this->setObject(new Account());
+        $this->setResourceName(self::RESOURCE_NAME);
+    }
+
+    public function create($object, $query = null)
+    {
+        list(, , $body) = $this->HttpClient->performHttpRequest(
+            HttpClient::REQUEST_POST,
+            self::RESOURCE_NAME,
+            null,
+            $object->loadToJson()
+        );
+
+        return $this->processRequest($body);
+    }
+
+    public function getList($parameters = [])
+    {
+        list($status, , $body) = $this->HttpClient->performHttpRequest(
+            HttpClient::REQUEST_GET,
+            self::RESOURCE_NAME,
+            $parameters
+        );
+
+        if ($status !== 200) {
+            return $this->processRequest($body);
+        }
+
+
+        $response = json_decode($body, true);
+
+        foreach ($response as &$item) {
+            $item = $this->getObject()->loadFromArray($item);
+        }
+
+        return $response;
+    }
+
+    public function update($object, $id)
+    {
+        list(, , $body) = $this->HttpClient->performHttpRequest(
+            HttpClient::REQUEST_PATCH,
+            sprintf('%s/%s', self::RESOURCE_NAME, $id),
+            null,
+            $object->loadToJson()
+        );
+
+        return $this->processRequest($body);
+    }
+}
