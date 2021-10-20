5e2d83bc60b98165606e27f453b6277f5940a1db
Nadir Hamid <matrix.nad@gmail.com>
2021-10-20 16:45:37 -0600

    add channel ID

---
+++ app/database/migrations/2021_10_20_224404_add_channel_id_to_calls.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddChannelIdToCalls extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('calls', function (Blueprint $table) {
+            //
+            $table->string('channel_id')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('calls', function (Blueprint $table) {
+            //
+        });
+    }
+}
