cf45518612a36498e418990b44b9de97967c16c0
Nadir Hamid <matrix.nad@gmail.com>
2023-01-18 22:48:28 +0000

    add class for microservice api key

--- app/app/MicroserviceApiKey.php
+++ app/app/MicroserviceApiKey.php
@@ -4,11 +4,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class WorkspaceRoutingFlow extends Model {
+class MicroserviceApiKey extends Model {
   protected $dates = ['created_at', 'updated_at'];
 
   protected $guarded  = array('id');
-  protected $table = "workspaces_routing_flows";
+  protected $table = "microservice_api_keys";
 }
 
 
