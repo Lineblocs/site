8cfa44ddb5dbccf801ecc518799491b5775aeea5
Nadir Hamid <matrix.nad@gmail.com>
2023-04-25 04:21:02 +0000

    add theme column to users table

---
+++ app/database/migrations/2023_04_25_041938_add_theme_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddThemeToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('theme')->default('light');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
