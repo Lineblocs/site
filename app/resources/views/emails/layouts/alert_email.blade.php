d0d58d589ad11712f4c03117b7a18404dd63c397
Nadir Hamid <matrix.nad@gmail.com>
2026-04-17 12:13:36 -0600

    Revert "Fix layout issues of all email templates"

--- app/resources/views/emails/layouts/alert_email.blade.php
+++ app/resources/views/emails/layouts/alert_email.blade.php
@@ -1,34 +1,98 @@
-@extends('emails.layouts.shell')
+<!DOCTYPE html>
 
-@section('email_body')
-  @include('emails.layouts.partials.header')
-  @yield('pre_title')
-  <tr>
-    <td class="email-top-space">&nbsp;</td>
-  </tr>
-  <tr>
-    <td>
-      <table width="100%" border="0" cellspacing="0" cellpadding="0" role="presentation" class="email-card">
-        <tr>
-          <td class="email-header-band mobile-pad">
-            <div class="email-header-title">@yield('title')</div>
-            @if (trim($__env->yieldContent('subtitle')) != '')
-              <div class="email-header-subtitle">@yield('subtitle')</div>
-            @endif
-          </td>
-        </tr>
-        <tr>
-          <td class="email-card-inner mobile-pad">
-            <table width="100%" border="0" cellspacing="0" cellpadding="0" role="presentation" class="email-body-wrap">
-              @yield('content')
-            </table>
-          </td>
-        </tr>
-      </table>
-    </td>
-  </tr>
-  @include('emails.layouts.partials.footer')
-  <tr>
-    <td class="email-bottom-space">&nbsp;</td>
-  </tr>
-@endsection
+<html lang="en" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
+    <head>
+        <title></title>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <!--[if mso]>
+            <xml>
+                <o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG /></o:OfficeDocumentSettings>
+            </xml>
+        <![endif]-->
+        <style>
+            * {
+                box-sizing: border-box;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+            }
+
+            a[x-apple-data-detectors] {
+                color: inherit !important;
+                text-decoration: inherit !important;
+            }
+
+            #MessageViewBody a {
+                color: inherit;
+                text-decoration: none;
+            }
+
+            p {
+                line-height: inherit;
+            }
+
+            .desktop_hide,
+            .desktop_hide table {
+                mso-hide: all;
+                display: none;
+                max-height: 0px;
+                overflow: hidden;
+            }
+
+            .image_block img + div {
+                display: none;
+            }
+
+            @media (max-width: 700px) {
+                .desktop_hide table.icons-inner,
+                .social_block.desktop_hide .social-table {
+                    display: inline-block !important;
+                }
+
+                .icons-inner {
+                    text-align: center;
+                }
+
+                .icons-inner td {
+                    margin: 0 auto;
+                }
+
+                .mobile_hide {
+                    display: none;
+                }
+
+                .row-content {
+                    width: 100% !important;
+                }
+
+                .stack .column {
+                    width: 100%;
+                    display: block;
+                }
+
+                .mobile_hide {
+                    min-height: 0;
+                    max-height: 0;
+                    max-width: 0;
+                    overflow: hidden;
+                    font-size: 0px;
+                }
+
+                .desktop_hide,
+                .desktop_hide table {
+                    display: table !important;
+                    max-height: none !important;
+                }
+            }
+        </style>
+    </head>
+    <body style="background-color: #f9f9f9; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+        <table border="0" cellpadding="0" cellspacing="0" class="nl-container" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f9f9f9;" width="100%">
+            @yield('content')
+        </table>
+        <!-- End -->
+    </body>
+</html>
--- app/resources/views/emails/layouts/newsltter.blade.php
+++
@@ -1,1 +0,0 @@
-@extends('emails.layouts.newsletter')
