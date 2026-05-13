ccc083a9c3974093d1c3f52c72e56df4339c76b7
gaurav-vaghela <gaurav.g1807@gmail.com>
2026-05-13 20:33:27 +0530

    Update plan_upgraded.blade.php

--- app/resources/views/emails/plan_upgraded.blade.php
+++ app/resources/views/emails/plan_upgraded.blade.php
@@ -13,22 +13,10 @@ Plan Upgraded
                     <td width="30" class="hide">&nbsp;</td>
                     <td class="mobilespacer2">
                         <table width="100%" border="0" cellspacing="0" cellpadding="0">
-
                             <tbody>
                                 <tr>
-                                    <td valign="top" height="20"
-                                        style="mso-line-height-rule:exactly;font-size:1px;line-height:20px;">&nbsp;</td>
-                                </tr>
-
-                                <tr>
-                                    <td align="left" valign="top"
-                                        style="font-family:'Roboto', Arial, Helvetica, sans-serif; font-size:14px;line-height:23px;color:#252f5a; text-align: center;">
-                                        This email confirms you have upgraded your plan
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="line-height: 30px; mso-line-height-rule: exactly; font-size: 0;"
-                                        height="30">&nbsp;</td>
+                                    <td valign="top" height="10"
+                                        style="mso-line-height-rule:exactly;font-size:1px;line-height:10px;">&nbsp;</td>
                                 </tr>
                                 <tr>
                                     <td bgcolor="#ffffff" valign="top" class="vspacer10" height="3"
@@ -41,34 +29,76 @@ Plan Upgraded
                                 </tr>
                                 <tr>
                                     <td valign="top">
-                                        <table width="100%" border="0" cellspacing="0" cellpadding="0"
-                                            class="SubContainer" style="border-radius: 3px;">
+                                        <table border="0" cellpadding="0" cellspacing="0"
+                                            class="paragraph_block block-1" role="presentation"
+                                            style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
+                                            width="100%">
+                                            <tr>
+                                                <td class="pad"
+                                                    style="padding-bottom: 15px; padding-left: 30px; padding-right: 30px; padding-top: 10px;">
+                                                    <div
+                                                        style="color: #393d47; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; font-size: 16px; line-height: 150%; text-align: left; mso-line-height-alt: 24px;">
+                                                        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="margin-bottom: 18px;">
+                                                            <tr>
+                                                                <td width="58" valign="top">
+                                                                    <table border="0" cellpadding="0" cellspacing="0" width="48" height="48" style="background-color:#ecfdf3; border-radius:24px;">
+                                                                        <tr>
+                                                                            <td align="center" valign="middle" style="font-family: Arial, Helvetica, sans-serif; font-size: 27px; line-height: 27px; color:#16a34a;">
+                                                                                &#10003;
+                                                                            </td>
+                                                                        </tr>
+                                                                    </table>
+                                                                </td>
+                                                                <td valign="middle" style="font-family: Arial, Helvetica Neue, Helvetica, sans-serif; color:#1f2937;">
+                                                                    <p style="margin: 0; font-size: 22px; line-height: 28px; font-weight: 700; word-break: break-word;">
+                                                                        Your plan has been upgraded
+                                                                    </p>
+                                                                    <p style="margin: 4px 0 0; font-size: 14px; line-height: 21px; color:#6b7280; word-break: break-word;">
+                                                                        The new plan is active on your workspace.
+                                                                    </p>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
 
-                                            <tbody>
-                                                <tr>
-                                                    <td>
-                                                        <table align="left" width="100%" border="0" cellspacing="0"
-                                                            cellpadding="0">
-                                                            <tbody>
-                                                                <tr>
-                                                                    <td width="50%" align="left"
-                                                                        style="font-family:'Roboto', Arial, Helvetica, sans-serif; font-size:12px;line-height:23px;color:#252f5a; text-align: left;">
-                                                                        New Plan
-                                                                    </td>
-                                                                    <td width="50%" align="left"
-                                                                        style="font-family:'Roboto', Arial, Helvetica, sans-serif; font-size:12px;line-height:23px;color:#252f5a; font-weight: bold; text-align: left;">
-                                                                        {{$plan}}
-                                                                    </td>
-                                                                </tr>
-                                                </tr>
+                                                        <p style="margin: 0; word-break: break-word;">
+                                                            Hello {{ isset($user) && trim($user->getName()) !== '' ? trim($user->getName()) : 'there' }},
+                                                        </p>
+                                                        <p style="margin: 12px 0 0; word-break: break-word;">
+                                                            This email confirms that your workspace plan has been upgraded successfully.
+                                                        </p>
 
-                                            </tbody>
+                                                        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="margin-top: 18px; border: 1px solid #e5e7eb; border-radius: 6px;">
+                                                            <tr>
+                                                                <td width="38%" style="padding: 14px 16px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; font-size: 14px; line-height: 22px; color:#6b7280; border-bottom: 1px solid #e5e7eb;">
+                                                                    New Plan
+                                                                </td>
+                                                                <td width="62%" style="padding: 14px 16px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; font-size: 15px; line-height: 22px; color:#1f2937; font-weight: 700; border-bottom: 1px solid #e5e7eb;">
+                                                                    {{ $plan }}
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td width="38%" style="padding: 14px 16px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; font-size: 14px; line-height: 22px; color:#6b7280;">
+                                                                    Status
+                                                                </td>
+                                                                <td width="62%" style="padding: 14px 16px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; font-size: 15px; line-height: 22px; color:#16a34a; font-weight: 700;">
+                                                                    Active
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+
+                                                        <p style="margin: 18px 0 0; word-break: break-word; color:#6b7280; font-size: 14px; line-height: 22px;">
+                                                            Your billing details will reflect the new plan according to your account settings.
+                                                        </p>
+                                                    </div>
+                                                </td>
+                                            </tr>
                                         </table>
                                     </td>
                                 </tr>
                             </tbody>
                         </table>
                     </td>
+                    <td width="30" class="hide">&nbsp;</td>
                 </tr>
                 <tr>
                     <td style="line-height: 1px; mso-line-height-rule: exactly; font-size: 0;" height="20">&nbsp;</td>
@@ -78,7 +108,6 @@ Plan Upgraded
                         style="mso-line-height-rule:exactly; font-size:1px; line-height:3px; border-top: 2px solid #f4f7fa;">
                         &nbsp;</td>
                 </tr>
-
                 <tr>
                     <td valign="top" height="20" style="mso-line-height-rule:exactly;font-size:1px;line-height:20px;">
                         &nbsp;</td>
@@ -86,7 +115,6 @@ Plan Upgraded
                 <tr>
                     <td style="line-height: 10px; mso-line-height-rule: exactly; font-size: 0;" height="10">&nbsp;</td>
                 </tr>
-
             </tbody>
         </table>
     </td>
