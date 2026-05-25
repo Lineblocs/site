f0306d93e68af23ad9c7aa379c2ff7c5ea2253fc
gaurav-vaghela <gaurav.g1807@gmail.com>
2026-05-25 21:42:25 +0530

    email template fixes

--- app/resources/views/emails/password.blade.php
+++ app/resources/views/emails/password.blade.php
@@ -1,10 +1,9 @@
 @extends('emails.layouts.header')
 @section('title')
-Congratulations
+Reset Your Password
 @endsection
 @section('content')
 
-<!-- Body Content One Column Start  -->
 <tr>
     <td valign="top" class="mobilespacer">
         <table width="100%" border="0" cellspacing="0" cellpadding="0">
@@ -13,93 +12,39 @@ Congratulations
                     <td width="30" class="hide">&nbsp;</td>
                     <td class="mobilespacer2">
                         <table width="100%" border="0" cellspacing="0" cellpadding="0">
-
                             <tbody>
                                 <tr>
-                                    <td valign="top" height="20"
-                                        style="mso-line-height-rule:exactly;font-size:1px;line-height:20px;">&nbsp;</td>
+                                    <td valign="top" height="12" style="mso-line-height-rule:exactly;font-size:1px;line-height:12px;">&nbsp;</td>
                                 </tr>
+                                <tr>
+                                    <td bgcolor="#ffffff" valign="top" height="3" style="mso-line-height-rule:exactly;font-size:1px;line-height:3px;border-top:2px solid #f4f7fa;">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td valign="top" style="padding:22px 30px 18px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;color:#393d47;font-size:16px;line-height:24px;text-align:left;">
+                                        <h2 style="margin:0 0 8px;font-size:24px;line-height:30px;color:#0a1247;font-weight:700;">Reset your password</h2>
+                                        <p style="margin:0;color:#5f6b7a;">We received a request to reset the password for your account.</p>
 
-                                <tr>
-                                    <td align="left" valign="top"
-                                        style="font-family:'Roboto', Arial, Helvetica, sans-serif; font-size:14px;line-height:23px;color:#252f5a; text-align: center;">
-                                        &nbsp;
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="line-height: 30px; mso-line-height-rule: exactly; font-size: 0;"
-                                        height="30">&nbsp;</td>
-                                </tr>
-                                <tr>
-                                    <td bgcolor="#ffffff" valign="top" class="vspacer10" height="3"
-                                        style="mso-line-height-rule:exactly; font-size:1px; line-height:3px; border-top: 2px solid #f4f7fa;">
-                                        &nbsp;</td>
-                                </tr>
-                                <tr>
-                                    <td valign="top" height="15"
-                                        style="mso-line-height-rule:exactly;font-size:1px;line-height:1px;">&nbsp;</td>
-                                </tr>
-                                <tr>
-                                    <td valign="top">
-
-                                        <table border="0" cellpadding="0" cellspacing="0"
-                                            class="paragraph_block block-1" role="presentation"
-                                            style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
-                                            width="100%">
+                                        <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="margin-top:20px;">
                                             <tr>
-                                                <td class="pad"
-                                                    style="padding-bottom: 10px; padding-left: 30px; padding-right: 30px; padding-top: 10px;">
-                                                    <div
-                                                        style="color: #393d47; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; font-size: 16px; line-height: 150%; text-align: left; mso-line-height-alt: 24px;">
-                                                        <p style="margin: 0; word-break: break-word;">
-                                                            <span>
-                                                                To reset your password, complete this form: <a
-                                                                    href="{{ \App\Helpers\MainHelper::createPortalLink('#/reset?token=' . $token } }}">Click
-                                                                    Here</a>
-                                                                <br>
-                                                <td align="left" valign="top"
-                                                    style="font-family:'Roboto', Arial, Helvetica, sans-serif; font-size:14px;line-height:23px;color:#252f5a; text-align: left;">
-                                                    This link will expire in {{ config('auth.reminder.expire', 60) }}
-                                                    minutes.
+                                                <td bgcolor="#0a1247" style="border-radius:4px;">
+                                                    <a href="{{ \App\Helpers\MainHelper::createPortalLink('#/reset?token=' . $token) }}" style="display:inline-block;padding:12px 20px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;font-size:15px;line-height:20px;color:#ffffff;text-decoration:none;font-weight:700;">Reset Password</a>
                                                 </td>
-                                                </span>
-                                                </p>
-                                                <p style="margin: 0; word-break: break-word;"></p>
-                                                <p style="margin: 0; word-break: break-word;">
-                                                </p>
-                                                </div>
+                                            </tr>
+                                        </table>
+
+                                        <p style="margin:18px 0 0;color:#5f6b7a;font-size:14px;line-height:22px;">This link will expire in {{ config('auth.reminder.expire', 60) }} minutes. If you did not request a password reset, you can safely ignore this email.</p>
                                     </td>
                                 </tr>
-                                <tr>
-
-                                </tr>
+                            </tbody>
                         </table>
-
                     </td>
+                    <td width="30" class="hide">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="line-height:1px;mso-line-height-rule:exactly;font-size:0;" height="16">&nbsp;</td>
                 </tr>
             </tbody>
         </table>
     </td>
-</tr>
-<tr>
-    <td style="line-height: 1px; mso-line-height-rule: exactly; font-size: 0;" height="20">&nbsp;</td>
-</tr>
-<tr>
-    <td bgcolor="#ffffff" valign="top" class="vspacer10" height="3"
-        style="mso-line-height-rule:exactly; font-size:1px; line-height:3px; border-top: 2px solid #f4f7fa;">
-        &nbsp;</td>
-</tr>
-
-<tr>
-    <td valign="top" height="20" style="mso-line-height-rule:exactly;font-size:1px;line-height:20px;">
-        &nbsp;</td>
-</tr>
-<tr>
-    <td style="line-height: 10px; mso-line-height-rule: exactly; font-size: 0;" height="10">&nbsp;</td>
-</tr>
-
-</tbody>
-</table>
-</td>
 </tr>
 @endsection
